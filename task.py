989c6f8a063f49f65f393c22275d27de559bbcbb
AlessioAmodio <a.amodio24@studenti.unisa.it>
2026-05-29 12:14:03 +0200

    IV Sub-Task Mi arrendo, esce 4x4 in tutti i casi

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 import numpy
 import numpy as np
+from numpy.ma.core import concatenate
+
 
 # Controlla il file readme.md per i dettagli su ciascun sub-task
 
@@ -38,17 +40,33 @@ def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
 
 def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
     """Sub-task 4: Correlazione tra Matrici 2x2."""
-    pass
+    if(len(m1) != 2 or len(m1[0]) != 2 or len(m1[1]) != 2):
+        return ("La prima matrice non è 2x2")
+    if(len(m2) != 2 or len(m2[0]) != 2 or len(m2[1]) != 2):
+        return ("La seconda matrice non è 2x2")
+    arr1 = np.array(m1)
+    arr2 = np.array(m2)
+    ris=numpy.corrcoef(arr1, arr2)
+    return ris
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
-    pass
+    k=len(v1)
+    v2=[]
+    for i in range(k):
+        v2.append(np.sin(np.array(v1) * np.pi / 180. ))
+
+    for i in range(k):
+        v2.append(np.cos(np.array(v1) * np.pi / 180.))
+
+    for i in range(k):
+    return (v2)
 
 
 def main():
     print("Sub-task 1:", prodotto_scalare([5, 7, 3], [4, 5, 6]))
     print("Sub-task 1:", rango_matrice([[2, 4, 1], [0, 1,0],[1,4,0]]))
-    print("Sub-task 3:", risolvi_sistema_lineare([[1, 0],[0,1],[2,3]], [3, 4,5]))
+    print("Sub-task 3:", risolvi_sistema_lineare([[1, 0],[0,1]], [3,4]))
     print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[2, 4], [6, 8]]))
     print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
 
